d8edecd0070bfd503dbc332cc2cb90b4b1c233ba
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-03 18:55:03 +0100

    Nueva clase - CameraService - Implementación de la funcionalidad de la captura y almacenamiento de imagenes definido en ICameraService #14

---
+++ IdCo/IdCo/Services/Camera/CameraService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace IdCo.Services.Camera
+{
+    /// <summary>
+    /// Controlar las principales funcionalidades de una camara Andorid.
+    /// </summary>
+    public class CameraService : ICameraService
+    {
+        /// <summary>
+        /// Inicializar el plugin para el uso de la camara.
+        /// </summary>
+        public CameraService()
+        {
+            CrossMedia.Current.Initialize();
+        }
+
+        /// <summary>
+        /// Establecer la forma de almacenamiento de las fotos.
+        /// </summary>
+        /// <param name="name"> Nombre del archivo de imagen </param>
+        /// <param name="photoSize"> Tamaño de la imagen </param>
+        /// <param name="directory"> Nombre del directorio de almacenamiento </param>
+        /// <returns> Opcion de almacenamiento para una imagen </returns>
+        public StoreCameraMediaOptions StoreCameraOptions(string name = null, PhotoSize photoSize = PhotoSize.Small, string directory = null)
+        {
+            if(name == null)
+            {
+                name = "photo_" + DateTime.Now.ToString();
+            }
+
+            if (directory == null)
+            {
+                directory = "IdCoGalery";
+            }
+            return new StoreCameraMediaOptions { Name = name, PhotoSize = photoSize, Directory = directory};
+        }
+
+        /// <summary>
+        /// Capturar una imagen con la camara, siempre y cuando la camara este disponible.
+        /// </summary>
+        /// <param name="storeOptions"></param>
+        /// <returns> Imagen capturada con la camara </returns>
+        public async Task<MediaFile> TakePhoto(StoreCameraMediaOptions storeOptions)
+        {
+            MediaFile photo;
+
+            if (!CrossMedia.Current.IsCameraAvailable)
+            {
+                throw new Exception("Error: La cámara no está disponible.");
+            }
+
+            photo = await CrossMedia.Current.TakePhotoAsync(storeOptions);
+            return photo;
+        }
+    }
+}
