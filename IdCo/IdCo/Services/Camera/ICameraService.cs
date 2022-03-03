511d135454fbd19f57d036a52dba677dec79f9d0
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-03 18:52:46 +0100

    ICameraService - Mejora en la definición de las funciones de la interfaz #14

--- IdCo/IdCo/Services/Camera/ICameraService.cs
+++ IdCo/IdCo/Services/Camera/ICameraService.cs
@@ -1,10 +1,26 @@
 ﻿using Plugin.Media.Abstractions;
+using System.Threading.Tasks;
 
 namespace IdCo.Services.Camera
 {
+    /// <summary>
+    /// Interfaz para el servicio de la Camara Android
+    /// </summary>
     public interface ICameraService
     {
-        StoreCameraMediaOptions StoreCameraOptions(string name, PhotoSize photoSize, string directory);
-        MediaFile TakePhoto();
+        /// <summary>
+        /// Forma de almacenamiento de las imagenes capturas.
+        /// </summary>
+        /// <param name="name"> Nombre del fichero de la foto capturada </param>
+        /// <param name="photoSize">Tamaño de la foto: Small, Medium, Large, Full, Custom, MaxWidthHeight </param>
+        /// <param name="directory"> Directorio de la imagen </param>
+        /// <returns></returns>
+        StoreCameraMediaOptions StoreCameraOptions(string name = null, PhotoSize photoSize = PhotoSize.Small, string directory = null);
+        /// <summary>
+        /// Captura de imagen con la camara.
+        /// </summary>
+        /// <param name="storeOptions"> Opciones de almacenamiento para las imagenes capturadas </param>
+        /// <returns></returns>
+        Task<MediaFile> TakePhoto(StoreCameraMediaOptions storeOptions);
     }
 }
