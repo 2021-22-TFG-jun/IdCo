cd084c23344c190f9250541c841e76d8acff7136
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-05 10:17:50 +0100

    UI - Funcionalidad CameraPage - Añadida la funcionalidad de la captura de imagenes con la cámara android al pulsar sobre el icono de la cámara #13

--- IdCo/IdCo/Views/CameraPage.xaml.cs
+++ IdCo/IdCo/Views/CameraPage.xaml.cs
@@ -1,18 +1,24 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using IdCo.Services.Camera;
+using Plugin.Media.Abstractions;
 
 namespace IdCo.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CameraPage : ContentPage
     {
+        ICameraService camera;
         public CameraPage()
         {
             InitializeComponent();
+            camera = new CameraService();
         }
 
         private async void CameraBtn_Clicked(object sender, System.EventArgs e)
         {
+            StoreCameraMediaOptions options = camera.StoreCameraOptions();
+            MediaFile photo = await camera.TakePhoto(options);
             await Navigation.PushAsync(new PersonPage());
         }
     }
