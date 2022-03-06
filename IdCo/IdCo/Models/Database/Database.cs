05c20c64bc1741585951c035dc00d626f0ac568e
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-06 13:05:24 +0100

    Nueva Clase - Database - Hereda de IDatabase #15

---
+++ IdCo/IdCo/Models/Database/Database.cs
@@ -0,0 +1,40 @@
+﻿namespace IdCo.Models.Database
+{
+    public class Database : IDatabase
+    {
+        public int Count()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DropTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int RemovePerson(Person.Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int SavePerson(Person.Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Person.Person SearchPersonById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Person.Person SearchPersonByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
