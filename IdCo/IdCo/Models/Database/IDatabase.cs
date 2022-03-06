dc13fb7eb3eafcadfee461c8c4a17af7c8dfed34
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-06 12:57:12 +0100

    Interfaz - IDatabase - Implementación de las operaciones sobre base de datos y tabla #15

--- IdCo/IdCo/Models/Database/IDatabase.cs
+++ IdCo/IdCo/Models/Database/IDatabase.cs
@@ -1,7 +1,46 @@
-﻿
-namespace IdCo.Models.Database
+﻿namespace IdCo.Models.Database
 {
+    /// <summary>
+    /// Interfaz para la Base de Datos.
+    /// </summary>
     public interface IDatabase
     {
+        /// <summary>
+        /// Crear una tabla sino existe en la BD
+        /// </summary>
+        void CreateTable();
+        /// <summary>
+        /// Eliminar una tabla de la BD, siempre y cuando esta exista.
+        /// </summary>
+        void DropTable();
+        /// <summary>
+        /// Contar el numero de elementos que tiene una tabla en una BD.
+        /// </summary>
+        /// <returns></returns>
+        int Count();
+        /// <summary>
+        /// Guardar una persona en la base de datos.
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        int SavePerson(Person.Person person);
+        /// <summary>
+        /// Eliminar una persona de la base de datos.
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        int RemovePerson(Person.Person person);
+        /// <summary>
+        /// Buscar una persona en funcion de su Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Person.Person SearchPersonById(int id);
+        /// <summary>
+        /// Buscar una persona en funcion de su nombre.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Person.Person SearchPersonByName(string name);
     }
 }
