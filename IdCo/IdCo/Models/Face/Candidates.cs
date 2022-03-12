63889de4cbb8a33b428e06186d3bee5896822c6d
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-12 11:11:14 +0100

    Nueva Clase - Candidates - Obtener los posibles candidatos y su nivel de confianza #17

---
+++ IdCo/IdCo/Models/Face/Candidates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IdCo.Models.Face
+{
+    public class Candidates
+    {
+        /// <summary>
+        /// Identificador de la persona candidata.
+        /// </summary>
+        public Guid PersonId { get; set; }
+        /// <summary>
+        /// Nivel de confianza en el candidato [0, 1].
+        /// </summary>
+        public double Confidence { get; set; }
+    }
+}
