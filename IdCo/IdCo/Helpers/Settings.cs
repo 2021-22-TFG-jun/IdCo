fc368f706962b41135cc50d1c095356dfe9d3ebc
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-12 12:01:47 +0100

    Clase - Settings - Añadidos datos necesarios para la configuración y acceso al servicio #18

--- IdCo/IdCo/Helpers/Settings.cs
+++ IdCo/IdCo/Helpers/Settings.cs
@@ -1,7 +1,18 @@
-﻿namespace IdCo.Helpers
+﻿using System;
+using System.IO;
+
+namespace IdCo.Helpers
 {
     public class Settings
     {
+        public static string Resource = ""; // Nombre de tu recurso
+        public static readonly string FaceEndPoint = "https://"+Resource+ ".cognitiveservices.azure.com/face/v1.0";
+        public static string FaceApiKey = ""; // Clave de acceso al recurso
 
+        public static readonly string FaceGroupID = "Conocidos";
+        public static readonly string FaceGroupName = "Mis Personas Conocidas";
+        public static readonly string FaceGroupDescription = "Agrupación de los rostros de mis personas conocidas";
+
+        public static readonly string BDName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PersonDB.db3");
     }
 }
