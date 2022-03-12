67c9498e977143c99ad8de41e631b31162386407
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-12 13:32:42 +0100

    Nueva Clase - PersonGroupService - Clase que implementa la inter IPersonGroupService #19

---
+++ IdCo/IdCo/Services/Face/PersonGroupService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace IdCo.Services.Face
+{
+    public class PersonGroupService : IPersonGroupService
+    {
+        public Task<TResponse> Create<TResponse>(string personGroupId, string name, string userData, string recognitionModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> Delete<TResponse>(string personGroupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> Train<TResponse>(string personGroupId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
