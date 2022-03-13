9cac47c0583060af8101cca7fc4c66475cd3166d
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-13 12:56:00 +0100

    Nueva Clase - PersonGroupPerson - Agrupar las características de un Person para realizar las peticiones http #19

--- IdCo/IdCo/Services/Face/PersonGroupPersonService.cs
+++ IdCo/IdCo/Services/Face/PersonGroupPersonService.cs
@@ -4,6 +4,20 @@ using System.Threading.Tasks;
 
 namespace IdCo.Services.Face
 {
+    /// <summary>
+    /// Clase que identifica las características obligatorias de una Persona.
+    /// </summary>
+    public class PersonGroupPerson
+    {
+        /// <summary>
+        /// Nombre de la persona
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Datos adicionales sobre la persona.
+        /// </summary>
+        public string UserData { get; set; }
+    }
     public class PersonGroupPersonService : IPersonGroupPersonService
     {
         public Task<string> AddFace(string personGroupId, string personId, string detectionModel, Stream photo)
