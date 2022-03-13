b7588c71682e39025aa1fd884c5fa3d20ab8025c
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-13 12:40:35 +0100

    Interfaz - IPersonGroupPersonService - Implementación de funciones #19

--- IdCo/IdCo/Services/Face/IPersonGroupPersonService.cs
+++ IdCo/IdCo/Services/Face/IPersonGroupPersonService.cs
@@ -1,6 +1,41 @@
-﻿namespace IdCo.Services.Face
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace IdCo.Services.Face
 {
     public interface IPersonGroupPersonService
     {
+        /// <summary>
+        /// Crear un Person en un PersonGroup.
+        /// </summary>
+        /// <param name="personGroupId">Id del PersonGroup</param>
+        /// <param name="name">Nombre del Person</param>
+        /// <param name="userData">Datos adicionales del Person</param>
+        /// <returns>200 OK: personId de la Person creada</returns>
+        Task<string> Create(string personGroupId, string name, string userData);
+        /// <summary>
+        /// Añadir un face a un Person ya creado.
+        /// </summary>
+        /// <param name="personGroupId">Id del PersonGroup</param>
+        /// <param name="personId">Id del Person</param>
+        /// <param name="detectionModel">Modelo de detección a usar</param>
+        /// <param name="photo">Imagen del rostro a vincular con el Person</param>
+        /// <returns>200 OK: persistedFaceId, id del rostro añadido</returns>
+        Task<string> AddFace(string personGroupId, string personId, string detectionModel, Stream photo);
+        /// <summary>
+        /// Eliminar un Person de un PersonGroup.
+        /// </summary>
+        /// <param name="personGroupId">Id del PersonGroup</param>
+        /// <param name="personId">Id del Person a eliminar</param>
+        /// <returns>200 OK</returns>
+        Task<string> Delete(string personGroupId, string personId);
+        /// <summary>
+        /// Eliminar un rostro de un Person.
+        /// </summary>
+        /// <param name="personGroupId">Id del PersonGroup</param>
+        /// <param name="personId">Id del Person</param>
+        /// <param name="faceId">Id del rostro a eliminar</param>
+        /// <returns>200 OK</returns>
+        Task<string> DeleteFace(string personGroupId, string personId, string faceId);
     }
 }
