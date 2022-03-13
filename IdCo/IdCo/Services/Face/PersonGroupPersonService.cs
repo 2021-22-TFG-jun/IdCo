a90990e01e5af5b3d670488f28392c6def72ad77
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-13 12:45:36 +0100

    Nueva Clase - PersonGroupPersonService - Clase que implementa la interfaz IPersonGroupPersonService #19

---
+++ IdCo/IdCo/Services/Face/PersonGroupPersonService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IdCo.Services.Face
+{
+    public class PersonGroupPersonService : IPersonGroupPersonService
+    {
+        public Task<string> AddFace(string personGroupId, string personId, string detectionModel, Stream photo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Create(string personGroupId, string name, string userData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Delete(string personGroupId, string personId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteFace(string personGroupId, string personId, string faceId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
