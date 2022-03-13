50eee916921372a06f42f28f17372c1d98a2a434
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-13 11:37:44 +0100

    Interfaz - IPersonGroupService - Modificación de los métodos #19

--- IdCo/IdCo/Services/Face/IPersonGroupService.cs
+++ IdCo/IdCo/Services/Face/IPersonGroupService.cs
@@ -7,27 +7,24 @@ namespace IdCo.Services.Face
         /// <summary>
         /// Crear un PersonGroup
         /// </summary>
-        /// <typeparam name="TResponse"></typeparam>
         /// <param name="personGroupId"></param>
         /// <param name="name"></param>
         /// <param name="userData"></param>
         /// <param name="recognitionModel"></param>
         /// <returns>200 OK</returns>
-        Task<TResponse> Create<TResponse>(string personGroupId, string name, string userData, string recognitionModel);
+        Task<string> Create(string personGroupId, string name, string userData, string recognitionModel);
         /// <summary>
         /// Entrenar un PersonGroup
         /// </summary>
-        /// <typeparam name="TResponse"></typeparam>
         /// <param name="personGroupId"></param>
         /// <returns>202 OK</returns>
-        Task<TResponse> Train<TResponse>(string personGroupId);
+        Task<string> Train(string personGroupId);
         /// <summary>
         /// Eliminar un PersonGroup
         /// </summary>
-        /// <typeparam name="TResponse"></typeparam>
         /// <param name="personGroupId"></param>
         /// <returns>200 OK</returns>
-        Task<TResponse> Delete<TResponse>(string personGroupId);
+        Task<string> Delete(string personGroupId);
 
     }
 }
