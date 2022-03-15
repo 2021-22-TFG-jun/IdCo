451b80c16018e4fc2e5388c7312b824e1cee0993
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-15 19:19:14 +0100

    Nueva interfaz - IFaceService - Detectar rostros/Identificar posibles coincidencias #19

---
+++ IdCo/IdCo/Services/Face/IFaceService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IdCo.Services.Face
+{
+    public interface IFaceService
+    {
+        /// <summary>
+        /// Detectar rostros.
+        /// </summary>
+        /// <param name="returnFaceId"></param>
+        /// <param name="photo"></param>
+        /// <returns></returns>
+        Task<Models.Face.Face[]> Detect(bool returnFaceId, Stream photo);
+        /// <summary>
+        /// Encontrar la coincidencia más cercana de un conjunto de rostros.
+        /// </summary>
+        /// <param name="facesIds"></param>
+        /// <param name="personGroupId"></param>
+        /// <param name="maxNumOfCandidates"></param>
+        /// <param name="confidenceThreshold"></param>
+        /// <returns></returns>
+        Task<Models.Face.Face[]> Identify(Guid[] facesIds, string personGroupId, int maxNumOfCandidates, double confidenceThreshold);
+    }
+}
