35c08e4c49442ac79d34f3fb9f963052b73415eb
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-17 17:52:39 +0100

    Nueva Clase - FaceService - Clase que implementa la interfaz IFaceService #19

---
+++ IdCo/IdCo/Services/Face/FaceService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IdCo.Services.Face
+{
+    public class FaceService : IFaceService
+    {
+        public Task<Models.Face.Face[]> Detect(bool returnFaceId, Stream photo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Models.Face.Face[]> Identify(Guid[] facesIds, string personGroupId, int maxNumOfCandidates, double confidenceThreshold)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
