7b04c576c47ab182dedb105cd8d9ff6b890dc4f2
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-03-22 19:41:13 +0100

    Funcionalidad - RecognitionPage - Modificado proceso en segundo plano y añadida la funcionalidad de detección e identificación de rostros #13

--- IdCo/IdCo/Views/RecognitionPage.xaml.cs
+++ IdCo/IdCo/Views/RecognitionPage.xaml.cs
@@ -9,6 +9,11 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.IO;
 
+using IdCo.Services.Face;
+using IdCo.Models.Face;
+using IdCo.Models.Person;
+using Xamarin.Essentials;
+
 namespace IdCo.Views
 {
 
@@ -16,20 +21,62 @@ namespace IdCo.Views
     public partial class RecognitionPage : ContentPage
     {
         BackgroundWorker tarea = null;
+        FaceService faceService;
+        bool procesado = false;
+        /// <summary>
+        /// Inicializador de la vista y del servicio.
+        /// </summary>
         public RecognitionPage()
         {
             InitializeComponent();
+            faceService = new FaceService();
         }
 
         /// <summary>
         /// Obtener la imagen capturada con sus propiedades (sin guardar en memoria)
-        /// Empezar a hacer las operaciones de deteccion e identificacion
+        /// Empezar a hacer las operaciones de deteccion e identificacion y búsqueda en BD.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void cameraView_MediaCaptured(object sender, MediaCapturedEventArgs e)
+        private async void cameraView_MediaCaptured(object sender, MediaCapturedEventArgs e)
         {
-            Console.WriteLine("MEDIA CAPTURED: ");
+            byte[] photo = e.ImageData.ToArray<byte>();
+            ImageSource sourceImg = e.Image;
+
+            using (var stream = new MemoryStream(photo))
+            {
+                Face[] detectFaces = await faceService.Detect(true, stream);
+                if (detectFaces.Any())
+                {
+                    Guid[] facesIds = new Guid[detectFaces.Length];
+                    int i = 0;
+                    foreach (Face face in detectFaces)
+                    {
+                        facesIds[i] = face.FaceId;
+                        i = i + 1;
+                    }
+
+                    Face[] identifyFaces = await faceService.Identify(facesIds);
+                    
+                    if (identifyFaces.Any())
+                    {
+                        foreach (var rostro in identifyFaces)
+                        {
+                            foreach(var candidato in rostro.Candidates)
+                            {
+                                Person person = App.Database.SearchPersonByPersonId(candidato.PersonId.ToString());
+                                await TextToSpeech.SpeakAsync(person.Name + " " + person.LastName);
+                            }
+                        }
+                        
+                    }                    
+                }
+                else
+                {
+                    Console.WriteLine("NO SE HA DETECTADO NINGUN ROSTRO");
+                }
+            }
+            procesado = true;
         }
         /// <summary>
         /// Volver a la vista inmediatamente anterior
@@ -39,12 +86,7 @@ namespace IdCo.Views
         private async void BackBtn_Clicked(object sender, EventArgs e)
         {
             if(tarea != null)
-            {
-                Console.WriteLine("Cancelando tarea");
                 tarea.CancelAsync();
-                tarea.Dispose();
-            }
-
             await Navigation.PopAsync();
         }
 
@@ -55,7 +97,8 @@ namespace IdCo.Views
         /// <param name="e"></param>
         private void StartBtn_Clicked(object sender, EventArgs e)
         {
-            BackgroudProcessJob(); 
+            BackgroudProcessJob();
+            tarea.RunWorkerAsync(2000);
         }
         /// <summary>
         /// Capturar una imagen
@@ -71,10 +114,9 @@ namespace IdCo.Views
         {
             tarea = new BackgroundWorker();
             tarea.WorkerSupportsCancellation = true;
-            tarea.DoWork += DoWork;
+            tarea.DoWork += new DoWorkEventHandler(DoWork);
+            tarea.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Finished);
             
-            tarea.RunWorkerAsync();
-            Console.ReadLine();
         }
         /// <summary>
         /// Tareas que debe realizar el proceso en segundo plano
@@ -83,29 +125,63 @@ namespace IdCo.Views
         /// <param name="eventArgs"></param>
         private void DoWork(object obj, DoWorkEventArgs eventArgs)
         {
-            Console.WriteLine("Iniciando trabajo...");
-            //System.Threading.Thread.Sleep(10000);
-            TakePhoto();
+            BackgroundWorker bw = obj as BackgroundWorker;
+
+            int arg = (int)eventArgs.Argument;
+            TakePhotoPeriodically(bw);
+
             if (tarea.CancellationPending)
             {
                 eventArgs.Cancel = true;
-                return;
             }
-            //System.Threading.Thread.Sleep(10000);
-            tarea.RunWorkerCompleted += Finished;
 
+        }
+        /// <summary>
+        /// Tomar fotos periodicamente
+        /// </summary>
+        /// <param name="bw"></param>
+        private void TakePhotoPeriodically(BackgroundWorker bw)
+        {
+            int result = 0;
+
+            while (!bw.CancellationPending)
+            {
+                procesado = false;
+                int i = 0;
+                TakePhoto();
+
+                while (!procesado && i < 3)
+                {
+                    System.Threading.Thread.Sleep(20000); // dar tiempo para procesar la imagen
+                    i = i + 1;
+                }
+                
+                result += 1;
+
+                if (result == 10)
+                    break;
+            }
         }
         /// <summary>
         /// Tareas de finalizacion del proceso de segundo plano
         /// </summary>
         /// <param name="obj"></param>
         /// <param name="e"></param>
-        private static void Finished(object obj, RunWorkerCompletedEventArgs e)
+        private void Finished(object obj, RunWorkerCompletedEventArgs e)
         {
-            if (e.Cancelled)
-                return;
 
-            Console.WriteLine("Trabajo terminado....");   
+            if (e.Cancelled)
+            {
+                Console.WriteLine("Tarea cancelada");
+            }
+            else if(e.Error != null)
+            {
+                Console.WriteLine("A ocurrido un error: " + e.Error.Message);
+            }
+            else
+            {
+                Console.WriteLine("Se ha terminado correctamente la tarea: " + e.Result);
+            }
         }
     }
 }
