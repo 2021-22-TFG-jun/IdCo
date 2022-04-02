d55401ee700a08d9ecc78bcc230319246f63591d
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-04-02 13:04:55 +0200

    Funcionalidad - DeletePersonPage - Acceso al recurso de Face API, obtener los principales datos de un persona (foto + nombre + apellido), bóton hacia atras, boton de basura (eliminar a la persona de la BD y del Face API) #21

--- IdCo/IdCo/Views/DeletePersonPage.xaml.cs
+++ IdCo/IdCo/Views/DeletePersonPage.xaml.cs
@@ -8,6 +8,8 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using IdCo.Models.Person;
+using System.IO;
+using IdCo.Services.Face;
 
 namespace IdCo.Views
 {
@@ -15,10 +17,47 @@ namespace IdCo.Views
     public partial class DeletePersonPage : ContentPage
     {
         Person person = null;
+        PersonGroupPersonService personGroupPersonService;
         public DeletePersonPage(Person person)
         {
             InitializeComponent();
             this.person = person;
+            personGroupPersonService = new PersonGroupPersonService();
+        }
+
+        /// <summary>
+        /// Cargar la persona seleccionada.
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            byte[] photo = person.Photo;
+            MemoryStream memoryStream = new MemoryStream(photo);
+            Image image = new Image { Source = ImageSource.FromStream(() => memoryStream) };
+
+            FacePhoto.Source = image.Source;
+            PersonNameLbl.Text = "Nombre: " + person.Name;
+            PersonLastnameLbl.Text = "Apellido: " + person.LastName;
+        }
+        /// <summary>
+        /// Volver a la pagina anterior sin realizar ninguna accion.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BackBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+        /// <summary>
+        /// Eliminar a la persona y volver a la pagina anterior.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void TrashBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Database.RemovePerson(person);
+            await personGroupPersonService.Delete(person.PersonId);
+
+            await Navigation.PopAsync();
         }
     }
 }
