f0f05d42024dfe9f8b5e98978fbfbcad61f92662
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-04-24 11:10:02 +0200

    Nueva actividad - StartActivity - Actividad de lanzamiento de la app donde se define la ventana de presentación #25

---
+++ IdCo/IdCo.Android/StartActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+
+
+namespace IdCo.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Start", Icon = "@drawable/logo", MainLauncher = true, NoHistory = true, Label = "Identificación de conocidos")]
+    public class StartActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            System.Threading.Thread.Sleep(500);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
