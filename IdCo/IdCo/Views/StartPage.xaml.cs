f91fd8d29f0a7ccbe8d6316da98b40ee46033997
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-05-01 12:35:24 +0200

    Clase - StartPage - Añadidas las funciones de Creación, Lectura y comprobación de la existencia de un fichero con los datos de inicio de sesion en el recurso. #27

--- IdCo/IdCo/Views/StartPage.xaml.cs
+++ IdCo/IdCo/Views/StartPage.xaml.cs
@@ -4,6 +4,8 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using IdCo.Services.Face;
 using System.Threading.Tasks;
+using System.IO;
+using IdCo.Helpers;
 
 namespace IdCo.Views
 {
@@ -11,6 +13,7 @@ namespace IdCo.Views
     public partial class StartPage : ContentPage
     {
         PersonGroupService personGroupService;
+        string loginFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Login.txt");
         public StartPage()
         {
             InitializeComponent();
@@ -21,14 +24,22 @@ namespace IdCo.Views
 #endif
             if (!Application.Current.Properties.ContainsKey("ResourceName") || !Application.Current.Properties.ContainsKey("ResourceKey"))
             {
-                lbl_start.IsVisible = false;
-                LoginDataPanel.IsVisible = true;
+                //lbl_start.IsVisible = false;
+                //LoginDataPanel.IsVisible = true;
+                //return;
+                if (!ExistLoginDataFile())
+                {
+                    lbl_start.IsVisible = false;
+                    LoginDataPanel.IsVisible = true;
+                    return;
+                }
+                else
+                {
+                    ReadLoginDataFile();
+                }
             }
-            else
-            {
-                lbl_start.IsVisible = true;
-                LoginDataPanel.IsVisible = false;
-            }
+            lbl_start.IsVisible = true;
+            LoginDataPanel.IsVisible = false;
         }
         /// <summary>
         /// Comprobar si el campo del nombre del recurso esta completado.
@@ -73,6 +84,47 @@ namespace IdCo.Views
             return false;
         }
         /// <summary>
+        /// Crear un fichero para el almacenamiento de los datos de inicio de sesion en el recurso.
+        /// </summary>
+        private void CreateLoginDataFile()
+        {
+            string resourceName = (string)Application.Current.Properties["ResourceName"];
+            string resourceKey = (string)Application.Current.Properties["ResourceKey"];
+            if (!ExistLoginDataFile())
+            {
+                StreamWriter writer = new StreamWriter(loginFilePath);
+                writer.WriteLine(resourceName);
+                writer.WriteLine(resourceKey);
+                writer.Close();
+            }
+        }
+        /// <summary>
+        /// Leer el archivo de inicio de sesion si existe
+        /// </summary>
+        private void ReadLoginDataFile()
+        {
+            if (ExistLoginDataFile())
+            {
+                StreamReader reader = new StreamReader(loginFilePath);
+                Application.Current.Properties["ResourceName"] = reader.ReadLine();
+                Application.Current.Properties["ResourceKey"] = reader.ReadLine();
+                reader.Close();
+            }
+        }
+        /// <summary>
+        /// Determinar si existe el fichero de inicio de sesión.
+        /// </summary>
+        /// <returns></returns>
+        private bool ExistLoginDataFile()
+        {
+            
+            if (!File.Exists(loginFilePath))
+            {
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
         /// Pasar a la siguiente vista siempre y cuando se haya rellenado los campos
         /// obligatorios del recurso.
         /// </summary>
@@ -85,6 +137,7 @@ namespace IdCo.Views
 
             if (flag1 && flag2)
             {
+                CreateLoginDataFile();
                 personGroupService = new PersonGroupService();
                 string status = await personGroupService.Create();
                 if (string.Equals(status, "OK") || string.Equals(status, "Conflict"))
