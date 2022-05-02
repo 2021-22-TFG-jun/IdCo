4209a3888364c5e73ca9c46bf071532b8d4775f8
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-05-02 13:22:43 +0200

    Permisos - IdCo.Android/AssemblyInfo - Dar permisos de acceso y lectura al almacenamiento externo. #28

--- IdCo/IdCo.Android/Properties/AssemblyInfo.cs
+++ IdCo/IdCo.Android/Properties/AssemblyInfo.cs
@@ -28,7 +28,15 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
 
 // Permisos para la camara
 [assembly: UsesFeature("android.hardware.camera", Required = true)]
 [assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
+
+//// Controlar el debug
+//#if DEBUG
+//[assembly: Application(Debuggable = true)]
+//#else
+//[assembly: Application(Debuggable = false)]
+//#endif
